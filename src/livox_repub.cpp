7feb467ed9e96a54661050f120014f4760713eb7
Harshal Deshpande <harshal.deshpande@getpeppermint.co>
2021-09-21 16:27:15 +0530

    Formatted code

--- src/livox_repub.cpp
+++ src/livox_repub.cpp
@@ -1,22 +1,23 @@
+#include "livox_ros_driver/CustomMsg.h"
 #include <pcl_conversions/pcl_conversions.h>
 #include <sensor_msgs/PointCloud2.h>
-#include "livox_ros_driver/CustomMsg.h"
 
 typedef pcl::PointXYZINormal PointType;
 typedef pcl::PointCloud<PointType> PointCloudXYZI;
 
 ros::Publisher pub_pcl_out0, pub_pcl_out1;
-uint64_t TO_MERGE_CNT = 1; 
+uint64_t TO_MERGE_CNT = 1;
 constexpr bool b_dbg_line = false;
 std::vector<livox_ros_driver::CustomMsgConstPtr> livox_data;
-void LivoxMsgCbk1(const livox_ros_driver::CustomMsgConstPtr& livox_msg_in) {
+void LivoxMsgCbk1(const livox_ros_driver::CustomMsgConstPtr &livox_msg_in) {
   livox_data.push_back(livox_msg_in);
-  if (livox_data.size() < TO_MERGE_CNT) return;
+  if (livox_data.size() < TO_MERGE_CNT)
+    return;
 
   pcl::PointCloud<PointType> pcl_in;
 
   for (size_t j = 0; j < livox_data.size(); j++) {
-    auto& livox_msg = livox_data[j];
+    auto &livox_msg = livox_data[j];
     auto time_end = livox_msg->points.back().offset_time;
     for (unsigned int i = 0; i < livox_msg->point_num; ++i) {
       PointType pt;
@@ -25,8 +26,11 @@ void LivoxMsgCbk1(const livox_ros_driver::CustomMsgConstPtr& livox_msg_in) {
       pt.z = livox_msg->points[i].z;
       float s = livox_msg->points[i].offset_time / (float)time_end;
 
-      pt.intensity = livox_msg->points[i].line +livox_msg->points[i].reflectivity /10000.0 ; // The integer part is line number and the decimal part is timestamp
-      pt.curvature = s*0.1;
+      pt.intensity = livox_msg->points[i].line +
+                     livox_msg->points[i].reflectivity /
+                         10000.0; // The integer part is line number and the
+                                  // decimal part is timestamp
+      pt.curvature = s * 0.1;
       pcl_in.push_back(pt);
     }
   }
@@ -43,7 +47,7 @@ void LivoxMsgCbk1(const livox_ros_driver::CustomMsgConstPtr& livox_msg_in) {
   livox_data.clear();
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
   ros::init(argc, argv, "livox_repub");
   ros::NodeHandle nh;
 
